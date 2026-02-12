27c746badfcdf36168384bf76796491978992ee3
44DerekWalker <44derekwalker@gmail.com>
2026-02-11 19:38:01 -0500

    Zombie Animations 2
    Added animations for when zombies get shot and attack

--- Assets/Scripts/Runtime/ZombieHealth.cs
+++ Assets/Scripts/Runtime/ZombieHealth.cs
@@ -45,6 +45,8 @@ public class ZombieHealth : MonoBehaviour {
     public float zombieMoveSpeed = 0.5f;
     public LineRenderer path;
     public int pathIndex = 0;
+    public float attackCooldown = 2.5f; // seconds between attacks
+    private float attackCooldownTimer = 0f;
     void Update() {
         if (!isAlive) {
             return;
@@ -55,7 +57,18 @@ public class ZombieHealth : MonoBehaviour {
         var pos = transform.position;
         var dir = path.transform.TransformPoint(path.GetPosition(pathIndex)) - pos;
         if (dir.magnitude < 0.1) {
-            if (pathIndex == path.positionCount) { // reached the end of path
+            if (pathIndex >= path.positionCount - 1) { // reached the end of path
+                //Attack Logic Here
+                if(attackCooldownTimer > 0) attackCooldownTimer -= Time.deltaTime;
+                if (attackCooldownTimer <= 0) {
+                    attackCooldownTimer = attackCooldown;
+                    int attackAnimation = Random.Range(1, 3);
+                    switch (attackAnimation) {
+                        case 1: anim.SetTrigger("Attack1"); break;
+                        case 2: anim.SetTrigger("Attack2"); break;
+                    }
+                }
+                
                 OnReach();
                 return;
             }
@@ -78,6 +91,9 @@ public class ZombieHealth : MonoBehaviour {
         hitPoint -= healthCollider.dmgMultiplier * damage;
         if (hitPoint <= 0) {
             OnDeath();
+        } else {
+            StartCoroutine(PlayHitReaction());
+            anim.SetTrigger("Hit");
         }
     }
     public void OnDeath() {
@@ -91,7 +107,15 @@ public class ZombieHealth : MonoBehaviour {
     }
     public void OnReach() {
         CurrencyManager.Instance.RemoveCurrency(50);
-        transform.DORotate(new(-90, 180, 0), 1f);
-        isAlive = false;
+        
+        //transform.DORotate(new(-90, 180, 0), 1f);
+        //isAlive = false;
+    }
+    
+    IEnumerator PlayHitReaction()
+    {
+        anim.SetLayerWeight(1, 0.8f); // hit layer
+        yield return new WaitForSeconds(0.15f);
+        anim.SetLayerWeight(1, 0f);
     }
 }
