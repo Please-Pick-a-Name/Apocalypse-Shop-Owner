b82e24fb7847a6dd8bfded95e5725f49c1e64674
zF1R3x <mingchun.andrew@gmail.com>
2026-03-12 17:28:15 -0400

    audio slider

--- Assets/Scripts/Manager/SoundMIxerManager.cs
+++ Assets/Scripts/Manager/SoundMIxerManager.cs
@@ -6,13 +6,13 @@ public class SoundMixerManager : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
     public void setMasterVolume(float level) {
-        audioMixer.SetFloat("MasterVolume", level);
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(level)* 20f);
     }
     public void setSoundFXVolume(float level) {
-        audioMixer.SetFloat("SoundFXVolume", level);
+        audioMixer.SetFloat("SoundFXVolume", Mathf.Log10(level)* 20f);
     }
     public void setMusicVolume(float level) {
-        audioMixer.SetFloat("MusicVolume", level);
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(level)* 20f);
     }
 
 }
