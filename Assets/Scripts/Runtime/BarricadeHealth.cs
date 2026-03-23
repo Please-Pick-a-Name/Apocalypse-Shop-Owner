5bf77580fa57f2b62cca713e6ad6382f212cae6c
zF1R3x <mingchun.andrew@gmail.com>
2026-03-23 08:39:47 -0400

    4 barricade states + repair barricade in shop

--- Assets/Scripts/Runtime/BarricadeHealth.cs
+++ Assets/Scripts/Runtime/BarricadeHealth.cs
@@ -2,29 +2,52 @@ using UnityEngine;
 using UnityEngine.UI;
 public class BarricadeHealth : MonoBehaviour
 {
-    public float health = 100f;
+    public float health = 75f;
     public float maxHealth = 100f;
     [SerializeField] public Slider slider;
+
+    public GameObject[] visualStates;
     void Start()
     {
-        health = maxHealth;
-        UpdateHealthBar(health, maxHealth);
+        UpdateVisuals();
     }
     void Update()
     {
-        slider.transform.rotation = Camera.main.transform.rotation;
-    }
-    public void UpdateHealthBar(float currentValue, float maxValue)
-    {
-        slider.value = currentValue / maxValue;
+        if (slider != null && Camera.main != null)
+            slider.transform.rotation = Camera.main.transform.rotation;
+        UpdateVisuals();
     }
     public void TakeDamage(float damageAmount)
     {
         health -= damageAmount;
-        UpdateHealthBar(health, maxHealth);
-        if (health <= 0)
-        {
+        UpdateVisuals();
+    }
+    public void Heal(float healAmount) {
+        health = Mathf.Min(maxHealth, health + healAmount);
+        UpdateVisuals();
+    }
+    private void UpdateVisuals() {
+        if (slider != null) slider.value = health/maxHealth;
 
+        float ratio = health / maxHealth;
+        int stateIndex = Mathf.FloorToInt(ratio * 4);
+        stateIndex = Mathf.Clamp(stateIndex, 0, 3);
+
+        if (stateIndex > 3) stateIndex = 3;
+        if (stateIndex < 0) stateIndex = 0;
+
+        foreach (GameObject obj in visualStates)
+        {
+            if (obj != null) obj.SetActive(false);
         }
+        
+        for (int i = 0; i < visualStates.Length; i++)
+        {
+            if (visualStates[stateIndex] != null)
+            {
+                visualStates[stateIndex].SetActive(true);
+            }
+        }
+
     }
 }
