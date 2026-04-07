b914fda8a892e018481898c4916e0c4fa6215189
44DerekWalker <44derekwalker@gmail.com>
2026-04-06 21:45:22 -0400

    dialogue item improvements
    -Added text that shows the item name when you pick it up
    -added attach transforms so the item has a better position in the hand when picked up

--- Assets/Scripts/Runtime/Dialogue/DialogueItem.cs
+++ Assets/Scripts/Runtime/Dialogue/DialogueItem.cs
@@ -1,11 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
 
 public class DialogueItem : MonoBehaviour {
 	public string itemID;
+	public TextMeshPro text;
+	
+	
+	private XRGrabInteractable grabInteractable;
 	public void OnItemDeliver() {
         // funny effects if we fancy
         Destroy(gameObject);
     }
+
+	private void Awake() {
+		grabInteractable = this.GetComponent<XRGrabInteractable>();
+		text.gameObject.SetActive(false);
+	}
+	
+	private void OnEnable()
+	{
+		if (grabInteractable != null)
+		{
+			grabInteractable.selectEntered.AddListener(OnGrabbed);
+			grabInteractable.selectExited.AddListener(OnReleased);
+		}
+	}
+
+	private void OnDisable()
+	{
+		if (grabInteractable != null)
+		{
+			grabInteractable.selectEntered.RemoveListener(OnGrabbed);
+			grabInteractable.selectExited.RemoveListener(OnReleased);
+		}
+	}
+
+	private void OnGrabbed(SelectEnterEventArgs args) {
+		text.gameObject.SetActive(true);
+	}
+
+	private void OnReleased(SelectExitEventArgs args) {
+		text.gameObject.SetActive(false);
+	}
+	
 }
