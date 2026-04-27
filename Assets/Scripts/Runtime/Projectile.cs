c6e078ae2926e24c05b960f540639fb55ce3a8c4
Please-Pick-a-Name <xmx526@gmail.com>
2026-04-26 21:02:07 -0400

    fix projectile line renderer start at wrong point

--- Assets/Scripts/Runtime/Projectile.cs
+++ Assets/Scripts/Runtime/Projectile.cs
@@ -17,6 +17,7 @@ public class Projectile : MonoBehaviour {
     public bool physicsEnabled = false;
 
     public void Init(Vector3 origin, Vector3 velocity, ProjectileOptions newProjectileOptions, ProjectileVisualOptions newVisualOptions) {
+        lastPos = origin;
         transform.position = origin;
         vel = velocity;
         projectileOptions = newProjectileOptions;
